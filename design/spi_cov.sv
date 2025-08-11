bc677147f1e22a9d760240f3fcb15422237f837f
user11@psdc_eda03 <user12@psdc-eda03.psdc.org.my>
2025-08-11 15:02:34 +0800

    driver and coverage update

--- design/spi_cov.sv
+++ design/spi_cov.sv
@@ -21,36 +21,36 @@ class spi_cov extends uvm_component;
     option.weight = 1;
     option.comment = "THIS IS MY FA_CG COVERAGE";
 
-    a_cp: coverpoint tr.a {
-      option.comment = "THIS IS MY FA_CG:A_CP COVERAGE";
-      option.weight = 1;
-    }
-    b_cp: coverpoint tr.b {
-      option.comment = "THIS IS MY FA_CG:B_CP COVERAGE";
-      option.weight = 1;
-    }
-    cin_cp: coverpoint tr.cin {
-      option.comment = "THIS IS MY FA_CG:CIN_CP COVERAGE";
-      option.weight = 1;
-    }
-    sum_cp: coverpoint tr.sum {
-      option.comment = "THIS IS MY FA_CG:SUM_CP COVERAGE";
-      option.weight = 1;
-    }
-    cout_cp: coverpoint tr.cout {
-      option.comment = "THIS IS MY FA_CG:COUT_CP COVERAGE";
-      option.weight = 1;
-    }
-    abcin_cp: cross a_cp, b_cp, cin_cp;
+//    a_cp: coverpoint tr.a {
+//      option.comment = "THIS IS MY FA_CG:A_CP COVERAGE";
+//      option.weight = 1;
+//    }
+//    b_cp: coverpoint tr.b {
+//      option.comment = "THIS IS MY FA_CG:B_CP COVERAGE";
+//      option.weight = 1;
+//    }
+//    cin_cp: coverpoint tr.cin {
+//      option.comment = "THIS IS MY FA_CG:CIN_CP COVERAGE";
+//      option.weight = 1;
+//    }
+//    sum_cp: coverpoint tr.sum {
+//      option.comment = "THIS IS MY FA_CG:SUM_CP COVERAGE";
+//      option.weight = 1;
+//    }
+//    cout_cp: coverpoint tr.cout {
+//      option.comment = "THIS IS MY FA_CG:COUT_CP COVERAGE";
+//      option.weight = 1;
+//    }
+//    abcin_cp: cross a_cp, b_cp, cin_cp;
   endgroup
 
   function void report_phase(uvm_phase phase);
-    `uvm_info("COVERAGE", $sformatf("Coverage spi_cg       : %.2f%%", spi_cg.get_coverage()), UVM_NONE)
-    `uvm_info("COVERAGE", $sformatf("Coverage a_cp        : %.2f%%", spi_cg.a_cp.get_coverage()), UVM_NONE)
-    `uvm_info("COVERAGE", $sformatf("Coverage b_cp        : %.2f%%", spi_cg.b_cp.get_coverage()), UVM_NONE)
-    `uvm_info("COVERAGE", $sformatf("Coverage cin_cp      : %.2f%%", spi_cg.cin_cp.get_coverage()), UVM_NONE)
-    `uvm_info("COVERAGE", $sformatf("Coverage sum_cp      : %.2f%%", spi_cg.sum_cp.get_coverage()), UVM_NONE)
-    `uvm_info("COVERAGE", $sformatf("Coverage cout_cp     : %.2f%%", spi_cg.cout_cp.get_coverage()), UVM_NONE)
-    `uvm_info("COVERAGE", $sformatf("Coverage abcin_cp    : %.2f%%", spi_cg.abcin_cp.get_coverage()), UVM_NONE)
+//\    `uvm_info("COVERAGE", $sformatf("Coverage spi_cg       : %.2f%%", spi_cg.get_coverage()), UVM_NONE)
+//\    `uvm_info("COVERAGE", $sformatf("Coverage a_cp        : %.2f%%", spi_cg.a_cp.get_coverage()), UVM_NONE)
+//\    `uvm_info("COVERAGE", $sformatf("Coverage b_cp        : %.2f%%", spi_cg.b_cp.get_coverage()), UVM_NONE)
+//\    `uvm_info("COVERAGE", $sformatf("Coverage cin_cp      : %.2f%%", spi_cg.cin_cp.get_coverage()), UVM_NONE)
+//\    `uvm_info("COVERAGE", $sformatf("Coverage sum_cp      : %.2f%%", spi_cg.sum_cp.get_coverage()), UVM_NONE)
+//\    `uvm_info("COVERAGE", $sformatf("Coverage cout_cp     : %.2f%%", spi_cg.cout_cp.get_coverage()), UVM_NONE)
+//\    `uvm_info("COVERAGE", $sformatf("Coverage abcin_cp    : %.2f%%", spi_cg.abcin_cp.get_coverage()), UVM_NONE)
   endfunction
 endclass
--- design/spi_drv.sv
+++ design/spi_drv.sv
@@ -1,8 +1,8 @@
-class fa_drv extends uvm_driver #(fa_tran);
-  `uvm_component_utils(fa_drv)
+class spi_drv extends uvm_driver #(spi_tran);
+  `uvm_component_utils(spi_drv)
 
-  virtual fadder_if vif;
-  uvm_analysis_port #(fa_tran) drv_ap;
+  virtual spi_if vif;
+  uvm_analysis_port #(spi_tran) drv_ap;
 
   function new(string name, uvm_component parent);
     super.new(name, parent);
@@ -11,29 +11,30 @@ class fa_drv extends uvm_driver #(fa_tran);
 
   function void build_phase(uvm_phase phase);
     super.build_phase(phase);
-    if(!uvm_config_db#(virtual fadder_if)::get(this, "", "vif", vif)) begin
-      `uvm_error("DRIVER", "Virtual interface (fadder_if) not found in config db")
+    if(!uvm_config_db#(virtual spi_if)::get(this, "", "vif", vif)) begin
+      `uvm_error("DRIVER", "Virtual interface (spi_if) not found in config db")
     end
   endfunction
 
   task run_phase(uvm_phase phase);
-    fa_tran tr;
+    spi_tran tr;
     forever begin
       seq_item_port.get_next_item(tr);
-
+//
       @(vif.drv_cb);
-      uvm_config_db#(int)::set(null, "*", "tran_count", tr.seq_count);
-      uvm_config_db#(int)::set(null, "*", "tran_index", tr.seq_index);
-      uvm_config_db#(string)::set(null, "*", "tran_type", tr.seq_type);
-
-      `uvm_info("DRIVER", $sformatf("Drive %0d/%0d %s tran to DUT: a=%0b, b=%0b, cin=%0b",
-                                    tr.seq_index, tr.seq_count, tr.seq_type, tr.a, tr.b, tr.cin),
-                                    UVM_MEDIUM)
-
-      vif.drv_cb.a_tb   <= tr.a;      // Drive 'a' from transaction (tr) to DUT
-      vif.drv_cb.b_tb   <= tr.b;      // Drive 'b' from transaction (tr) to DUT
-      vif.drv_cb.cin_tb <= tr.cin;  // Drive 'cin' from transaction (tr) to DUT
-
+	`uvm_info(get_type_name(), "Good news for seeing this", UVM_LOW)
+//      uvm_config_db#(int)::set(null, "*", "tran_count", tr.seq_count);
+//      uvm_config_db#(int)::set(null, "*", "tran_index", tr.seq_index);
+//      uvm_config_db#(string)::set(null, "*", "tran_type", tr.seq_type);
+//
+//      `uvm_info("DRIVER", $sformatf("Drive %0d/%0d %s tran to DUT: a=%0b, b=%0b, cin=%0b",
+//                                    tr.seq_index, tr.seq_count, tr.seq_type, tr.a, tr.b, tr.cin),
+//                                    UVM_MEDIUM)
+//
+//      vif.drv_cb.a_tb   <= tr.a;      // Drive 'a' from transaction (tr) to DUT
+//      vif.drv_cb.b_tb   <= tr.b;      // Drive 'b' from transaction (tr) to DUT
+//      vif.drv_cb.cin_tb <= tr.cin;  // Drive 'cin' from transaction (tr) to DUT
+//
       seq_item_port.item_done();
     end
   endtask
