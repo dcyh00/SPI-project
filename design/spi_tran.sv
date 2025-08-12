a52fcf04c56037882fb73e86869ade941edeb085
user16 <user16@psdc-eda04.psdc.org.my>
2025-08-12 15:18:02 +0800

    Revert the start_bit to start

--- design/spi_tran.sv
+++ design/spi_tran.sv
@@ -1,10 +1,10 @@
 class spi_tran extends uvm_sequence_item;
   rand bit rst_n;
-  rand bit start_bit;
+  rand bit start;
   rand bit miso;
   rand bit [7:0] tx_data;
   rand bit [3:0] cmd;
-	
+
   logic [7:0] rx_data;
   logic busy;
   logic done;
