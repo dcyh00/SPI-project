80481a831f39d4de327f74b0402c11627d46939e
user11@psdc_eda03 <user12@psdc-eda03.psdc.org.my>
2025-08-13 15:36:35 +0800

    test up

---
+++ design/test/spi_reset_test.sv
@@ -0,0 +1,56 @@
+class spi_reset_test extends spi_test;
+  `uvm_component_utils(spi_reset_test)
+
+	protocol_seq seq;
+
+  function new(string name, uvm_component parent);
+    super.new(name, parent);
+  endfunction
+
+  task run_phase(uvm_phase phase);
+    seq = protocol_seq::type_id::create("seq");
+
+
+    phase.raise_objection(this);
+    	
+	fork
+	
+	begin
+		vif.rst_n <= 1'b0;
+		repeat(5) @(posedge vif.clk);
+
+		vif.rst_n <= 1'b1; 
+		repeat(10) @(posedge vif.clk);
+		vif.start <= 1'b1;
+		repeat(10) @(posedge vif.clk);
+		vif.start <= 1'b0;
+		vif.rst_n <= 1'b0;
+		repeat(10) @(posedge vif.clk);
+
+		vif.rst_n <= 1'b1; 
+		repeat(10) @(posedge vif.clk);
+
+		
+		seq.randomize() with {cmd==1; tx_data == 8'hFF;};
+    		seq.start(env.agt.sqr);
+		seq.randomize();
+    		seq.start(env.agt.sqr);
+		seq.randomize();
+    		seq.start(env.agt.sqr);
+	end
+	
+	begin
+		seq.randomize() with {cmd==1; tx_data == 8'hA5;};
+    		seq.start(env.agt.sqr);
+	end
+
+	join
+
+	#1000ns;
+    phase.drop_objection(this);
+  endtask
+
+endclass
+
+
+
