94e0597880459e93009f01f040f6e0a3b4eff6b1
user16 <user16@psdc-eda04.psdc.org.my>
2025-08-13 15:35:02 +0800

    update covergroup

--- design/spi_cov.sv
+++ design/spi_cov.sv
@@ -19,38 +19,81 @@ class spi_cov extends uvm_component;
   covergroup spi_cg with function sample(spi_tran tr);
     option.per_instance = 1;
     option.weight = 1;
-    option.comment = "THIS IS MY FA_CG COVERAGE";
+    option.comment = "THIS IS MY SPI_CG COVERAGE";
 
-//    a_cp: coverpoint tr.a {
-//      option.comment = "THIS IS MY FA_CG:A_CP COVERAGE";
-//      option.weight = 1;
-//    }
-//    b_cp: coverpoint tr.b {
-//      option.comment = "THIS IS MY FA_CG:B_CP COVERAGE";
-//      option.weight = 1;
-//    }
-//    cin_cp: coverpoint tr.cin {
-//      option.comment = "THIS IS MY FA_CG:CIN_CP COVERAGE";
-//      option.weight = 1;
-//    }
-//    sum_cp: coverpoint tr.sum {
-//      option.comment = "THIS IS MY FA_CG:SUM_CP COVERAGE";
-//      option.weight = 1;
-//    }
-//    cout_cp: coverpoint tr.cout {
-//      option.comment = "THIS IS MY FA_CG:COUT_CP COVERAGE";
-//      option.weight = 1;
-//    }
-//    abcin_cp: cross a_cp, b_cp, cin_cp;
+    RST_N_CP: coverpoint tr.rst_n {
+      option.comment = "THIS IS MY_SPI_CG:RST_N_CP COVERAGE";
+      option.weight  = 1;
+      bins low  = {0};
+      bins high = {1};
+    }
+    START_CP: coverpoint tr.start {
+      option.comment = "THIS IS MY_SPI_CG:START_CP COVERAGE";
+      option.weight  = 1;
+      bins low  = {0};
+      bins high = {1};
+    }
+    TX_DATA_CP: coverpoint tr.tx_data {
+      option.comment = "THIS IS MY_SPI_CG:TX_DATA_CP COVERAGE";
+      option.weight  = 1;
+      bins zero     = {0};
+      bins non_zero = {[8'h01:8'hFF]};
+    }
+    RX_DATA_CP: coverpoint tr.rx_data {
+      option.comment = "THIS IS MY_SPI_CG:RX_DATA_CP COVERAGE";
+      option.weight  = 1;
+      bins zero     = {0};
+      bins non_zero = {[8'h01:8'hFF]};
+    }
+    BUSY_CP: coverpoint tr.busy {
+      option.comment = "THIS IS MY_SPI_CG:BUSY_CP COVERAGE";
+      option.weight  = 1;
+      bins low  = {0};
+      bins high = {1};
+    }
+    DONE_CP: coverpoint tr.done {
+      option.comment = "THIS IS MY_SPI_CG:DONE_CP COVERAGE";
+      option.weight  = 1;
+      bins low  = {0};
+      bins high = {1};
+    }
+    SCLK_CP: coverpoint tr.sclk {
+      option.comment = "THIS IS MY_SPI_CG:SCLK_CP COVERAGE";
+      option.weight  = 1;
+      bins low  = {0};
+      bins high = {1};
+    }
+    MOSI_CP: coverpoint tr.mosi {
+      option.comment = "THIS IS MY_SPI_CG:MOSI_CP COVERAGE";
+      option.weight  = 1;
+      bins low  = {0};
+      bins high = {1};
+    }
+    MISO_CP: coverpoint tr.miso {
+      option.comment = "THIS IS MY_SPI_CG:MISO_CP COVERAGE";
+      option.weight  = 1;
+      bins low  = {0};
+      bins high = {1};
+    }
+    CS_N_CP: coverpoint tr.cs_n {
+      option.comment = "THIS IS MY_SPI_CG:CS_N_CP COVERAGE";
+      option.weight  = 1;
+      bins low  = {0};
+      bins high = {1};
+    }
   endgroup
 
   function void report_phase(uvm_phase phase);
-//\    `uvm_info("COVERAGE", $sformatf("Coverage spi_cg       : %.2f%%", spi_cg.get_coverage()), UVM_NONE)
-//\    `uvm_info("COVERAGE", $sformatf("Coverage a_cp        : %.2f%%", spi_cg.a_cp.get_coverage()), UVM_NONE)
-//\    `uvm_info("COVERAGE", $sformatf("Coverage b_cp        : %.2f%%", spi_cg.b_cp.get_coverage()), UVM_NONE)
-//\    `uvm_info("COVERAGE", $sformatf("Coverage cin_cp      : %.2f%%", spi_cg.cin_cp.get_coverage()), UVM_NONE)
-//\    `uvm_info("COVERAGE", $sformatf("Coverage sum_cp      : %.2f%%", spi_cg.sum_cp.get_coverage()), UVM_NONE)
-//\    `uvm_info("COVERAGE", $sformatf("Coverage cout_cp     : %.2f%%", spi_cg.cout_cp.get_coverage()), UVM_NONE)
-//\    `uvm_info("COVERAGE", $sformatf("Coverage abcin_cp    : %.2f%%", spi_cg.abcin_cp.get_coverage()), UVM_NONE)
+    `uvm_info("COVERAGE", $sformatf("Coverage spi_cg    : %.2f%%", spi_cg.get_coverage()), UVM_NONE)
+    `uvm_info("COVERAGE", $sformatf("Coverage rst_n     : %.2f%%", spi_cg.RST_N_CP.get_coverage()), UVM_NONE)
+    `uvm_info("COVERAGE", $sformatf("Coverage start_cp  : %.2f%%", spi_cg.START_CP.get_coverage()), UVM_NONE)
+    `uvm_info("COVERAGE", $sformatf("Coverage tx_data_cp: %.2f%%", spi_cg.TX_DATA_CP.get_coverage()), UVM_NONE)
+    `uvm_info("COVERAGE", $sformatf("Coverage rx_data_cp: %.2f%%", spi_cg.RX_DATA_CP.get_coverage()), UVM_NONE)
+    `uvm_info("COVERAGE", $sformatf("Coverage busy_cp   : %.2f%%", spi_cg.BUSY_CP.get_coverage()), UVM_NONE)
+    `uvm_info("COVERAGE", $sformatf("Coverage done_cp   : %.2f%%", spi_cg.DONE_CP.get_coverage()), UVM_NONE)
+    `uvm_info("COVERAGE", $sformatf("Coverage sclk_cp   : %.2f%%", spi_cg.SCLK_CP.get_coverage()), UVM_NONE)
+    `uvm_info("COVERAGE", $sformatf("Coverage mosi_cp   : %.2f%%", spi_cg.MOSI_CP.get_coverage()), UVM_NONE)
+    `uvm_info("COVERAGE", $sformatf("Coverage miso_cp   : %.2f%%", spi_cg.MISO_CP.get_coverage()), UVM_NONE)
+    `uvm_info("COVERAGE", $sformatf("Coverage cs_n_cp   : %.2f%%", spi_cg.CS_N_CP.get_coverage()), UVM_NONE)
   endfunction
 endclass
