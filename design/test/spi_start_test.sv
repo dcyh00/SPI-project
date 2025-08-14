2dcc4e91c456fff0ec24797c49b0e97e74b5d1d7
user11@psdc_eda03 <user12@psdc-eda03.psdc.org.my>
2025-08-14 14:02:07 +0800

    test up

--- design/test/spi_start_test.sv
+++ design/test/spi_start_test.sv
@@ -14,31 +14,18 @@ class spi_start_test extends spi_test;
     phase.raise_objection(this);
 
 	fork
+		begin
+		seq.randomize() with {cmd==0;};
+    		seq.start(env.agt.sqr);
+		end
 
-	begin
-		vif.rst_n <= 1'b0;
-		repeat(5) @(posedge vif.clk);
-
-		vif.rst_n <= 1'b1;
-		repeat(5) @(posedge vif.clk);
-
-		vif.start = 1'b0;
-		repeat(3) @(posedge vif.clk);//wait out of reset & stable input
+		begin
+		repeat(20) @(posedge vif.clk);
 		vif.start = 1'b1;
-		repeat(3) @(posedge vif.clk);
-		vif.start = 1'b0;
-		repeat(3) @(posedge vif.clk);
-		vif.start = 1'b1;
-		seq.randomize() with {cmd==1; tx_data == 8'hFF;};
-		seq.start(env.agt.sqr);
 		repeat(10) @(posedge vif.clk);
 		vif.start = 1'b0;
-	end
+		end
 
-	begin
-		seq.randomize() with {cmd==1; tx_data == 8'hA5;};
-		seq.start(env.agt.sqr);
-	end
 
 	join
 
