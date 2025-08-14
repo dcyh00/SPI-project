dda26f56bce631a65b73b5769b6219f18c82c01e
user16 <user16@psdc-eda04.psdc.org.my>
2025-08-14 09:17:29 +0800

    Update assertion

--- design/test/spi_reset_test.sv
+++ design/test/spi_reset_test.sv
@@ -12,38 +12,36 @@ class spi_reset_test extends spi_test;
 
 
     phase.raise_objection(this);
-    	
+
 	fork
-	
-	begin
-		vif.rst_n <= 1'b0;
-		repeat(5) @(posedge vif.clk);
+		begin
+			vif.rst_n <= 1'b0;
+			repeat(5) @(posedge vif.clk);
 
-		vif.rst_n <= 1'b1; 
-		repeat(10) @(posedge vif.clk);
-		vif.start <= 1'b1;
-		repeat(10) @(posedge vif.clk);
-		vif.start <= 1'b0;
-		vif.rst_n <= 1'b0;
-		repeat(10) @(posedge vif.clk);
+			vif.rst_n <= 1'b1;
+			repeat(10) @(posedge vif.clk);
+			vif.start <= 1'b1;
+			repeat(10) @(posedge vif.clk);
+			vif.start <= 1'b0;
+			vif.rst_n <= 1'b0;
+			repeat(10) @(posedge vif.clk);
 
-		vif.rst_n <= 1'b1; 
-		repeat(10) @(posedge vif.clk);
+			vif.rst_n <= 1'b1;
+			repeat(10) @(posedge vif.clk);
 
-		
-		seq.randomize() with {cmd==1; tx_data == 8'hFF;};
-    		seq.start(env.agt.sqr);
-		seq.randomize();
-    		seq.start(env.agt.sqr);
-		seq.randomize();
-    		seq.start(env.agt.sqr);
-	end
-	
-	begin
-		seq.randomize() with {cmd==1; tx_data == 8'hA5;};
-    		seq.start(env.agt.sqr);
-	end
 
+			seq.randomize() with {cmd==1; tx_data == 8'hFF;};
+			seq.start(env.agt.sqr);
+			seq.randomize();
+			seq.start(env.agt.sqr);
+			seq.randomize();
+			seq.start(env.agt.sqr);
+		end
+
+		begin
+			seq.randomize() with {cmd==1; tx_data == 8'hA5;};
+			seq.start(env.agt.sqr);
+		end
 	join
 
 	#1000ns;
